0b5e13437ef546c7c5e9868014d48f50592a79ea
locpt109 <phamtanloc1009@gmail.com>
2024-10-31 23:25:04 +0700

    created full service

---
+++ services/inventory-service/src/main/java/com/foodygo/inventory/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foodygo.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ services/user-service/src/main/java/com/foodygo/user/entity/User.java
@@ -0,0 +1,4 @@
+package com.foodygo.user.entity;
+
+public class User {
+}
